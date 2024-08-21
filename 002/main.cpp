4c3404d3cd40a620cbe79850fe839db2e119a753
Diego Tavares <mello.gredy@gmail.com>
2024-08-20 23:38:37 -0300

    :bulb: Variável
    Explorando a memória do computador.

--- 002/main.cpp
+++ 002/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!";
+
+//a a ilustração entre colchetes abaixo representa a memória do computador 
+  /*[2] [3] [4] [5] [(int), (idade), (26)] [] [] [] [] */
+
+  
+    int idade;
+    idade = 26;
+    //aqui, a variável idade recebe o valor 26
     return 0;
 }
