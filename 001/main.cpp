fe18f96583407647631ee4a4b0597eb0004623a4
Diego Tavares <mello.gredy@gmail.com>
2024-08-20 23:31:46 -0300

    Initial commit

---
+++ 001/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl << "I am ready for CS16!" << endl << "I am ready for CS16!" << endl;
+    return 0;
+}
+
+
+
