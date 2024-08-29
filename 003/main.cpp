ca8db7c9bc5a6520994771d7f499aea17b733db9
Diego Tavares <mello.gredy@gmail.com>
2024-08-28 22:54:15 -0300

    :bulb: Operadores Básicos

---
+++ 003/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // 30 + 30; --> soma
+  // 30 - 30; --> subtração
+  // 30 * 30; --> multiplicação
+  // 30 / 30; --> divisão
+
+  int soma = 30 + 30;
+  int subtracao = 30 - 30;
+  int multiplicacao = 30 * 30;
+  int divisao = 30 / 30;
+
+  cout << "Soma: " << soma << endl;
+  cout << "Subtração: " << subtracao << endl;
+  cout << "Multiplicação: " << multiplicacao << endl;
+  cout << "Divisão: " << divisao << endl;
+  
+  return 0;
+}
+
